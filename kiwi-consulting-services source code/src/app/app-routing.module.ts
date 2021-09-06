2fa256bc54f3c21424b494530727b27da082c432
antoniojoboy <github@actilogic.com.au>
2021-09-06 21:18:28 +0800

    asdf

---
+++ kiwi-consulting-services source code/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
