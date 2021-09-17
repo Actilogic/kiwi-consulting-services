435626a8044540bd2f18a83ff1f4c0da84182bd2
antoniojoboy <89784839+Actilogic@users.noreply.github.com>
2021-09-18 02:22:46 +0800

    e

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5c5020338d1aee010a46.js"
+    "url": "webpack-runtime-2e8ad5084163836e1df5.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "096afa0457cafebad7c02997b8c31751"
+    "revision": "5bcf9e7af594fb874b85e374c08ccc93"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0ebfce11b181451592346af035303c39"
+    "revision": "f765d69c115783d4c17de22c164ffd93"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
